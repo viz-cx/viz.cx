a2535692d066ebc0edaef5dfa382ba580567cbc5
Vladimir Babin <vovababin@gmail.com>
2022-05-02 02:21:07 +0500

    Check data exists

--- server/processors/processVoiceProtocol.ts
+++ server/processors/processVoiceProtocol.ts
@@ -6,21 +6,26 @@ var slugify = require('slugify')
 export async function processVoiceProtocol() {
     try {
         const lastSavedBlock = await getLastSavedPostsBlock()
+        const randomLimit = Math.floor(Math.random() * 10) + 1
         const newPosts = await OpInBlockModel
             .find({
                 "type": "custom",
                 "obj.id": "V",
+                "obj.d": { $exists: true },
                 "block": { $gt: lastSavedBlock }
             })
-            .limit(1)
+            .limit(randomLimit)
         const mongoPosts = newPosts.flatMap(post => {
             const customObject = post.obj as BlockchainCustom
             let json = JSON.parse(customObject.json)
             let voicePost = json as MongoVoicePost
+            if (!voicePost.d) {
+                return null
+            }
             voicePost.block = post.block
             voicePost.author = customObject.required_regular_auths[0]
             let title = ""
-            if (voicePost.d && typeof voicePost.d.t !== 'undefined') {
+            if (typeof voicePost.d.t !== 'undefined') {
                 title = voicePost.d.t
             }
             voicePost.title = makeTitle(title, voicePost.block)
