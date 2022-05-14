54adae73c7cb06b8c466645f947edbb009bcdc26
Vladimir Babin <vovababin@gmail.com>
2022-05-14 23:16:17 +0500

    Add sitemap

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import { startVizParsing } from './viz-parser'
 import { startMongo } from '../helpers/startMongo'
 require('dotenv').config()
 
-const host = process.env.HOST || "http://localhost"
+const url = process.env.SITE_URL || "http://localhost"
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -22,7 +22,7 @@ async function start() {
 
     // tslint:disable-next-line:no-console
     console.log(
-      `> Server listening at ${host}:${port} as ${dev ? 'development' : process.env.NODE_ENV
+      `> Server listening at ${url}:${port} as ${dev ? 'development' : process.env.NODE_ENV
       }`
     )
     startVizParsing()
