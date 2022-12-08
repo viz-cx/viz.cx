2e0f45353e75db34f88f8e2f923b279939ec1ca4
inov8 <blackisnewyellow@gmail.com>
2022-12-08 10:52:07 +0300

    Add start_parsing_by_block. Add some API funcs.

--- backend/main.py
+++ backend/main.py
@@ -6,45 +6,91 @@ import helpers.api
 from dotenv import load_dotenv; load_dotenv()
 from helpers.mongo import get_last_block_in_db
 from helpers.viz import viz
-from parser.parser import start_parsing
+from parser.parser import start_parsing, start_parsing_by_block
 
-thread = Thread(target=start_parsing, daemon=True, name='parser')
+thread = Thread(target=start_parsing_by_block, daemon=True, name='parser')
 thread.start()
 
 app = FastAPI(root_path=os.getenv('ROOT_PATH'))
 
 @app.get('/')
 def home():
-    # return viz.info()
-    return {'Hello': 'World'}
+    return viz.info()
+    # return {'Hello': 'World'}
 
 @app.get('/latest_block')
 def latest():
     return get_last_block_in_db()
 
 # Количество всех операций в блокчейне.
-@app.get('/count_all_tx')
-def count_all_tx():
-    return helpers.api.get_all_tx_number_in_db()
+@app.get('/count_tx/all')
+def count_all_tx() -> dict:
+    result = helpers.api.get_all_tx_number_in_db()
+    return {'transactions': result, 'operation type': 'all', 'date': 'all'}
 
 # Количество всех операций в блокчейне за заданный период с указанной даты.
-@app.get('/count_all_tx/{to_date_str}/{period_in_seconds}')
+@app.get('/count_tx/{to_date_str}/{period_in_seconds}')
 def count_all_tx_in_period(to_date:dt.datetime=dt.datetime.now(), 
-    period:dt.timedelta=dt.timedelta(hours=1)):
-    to_date_str = dt.datetime.strftime(to_date, '%Y-%m-%d')
+                            period:dt.timedelta=dt.timedelta(hours=1)) -> dict:
+    to_date_str = dt.datetime.strftime(to_date, '%Y-%m-%d %H:%M:%S')
+    from_date_str = dt.datetime.strftime(to_date - period, '%Y-%m-%d %H:%M:%S')
     period_in_seconds = period.seconds
-    return helpers.api.get_all_tx_number_in_db_in_period(to_date, period)
+    result = helpers.api.get_all_tx_number_in_db_in_period(to_date, period)
+    return {'transactions': result, 'operation type': 'all',
+            'date': {'from': from_date_str, 'to': to_date_str}}
+
+# Количество операций по заданному типу за всё время.
+@app.get('/count_tx/{operation_type}')
+def count_tx_by_op_type(operation_type:str="witness_reward") -> dict:
+    result = helpers.api.get_tx_number(operation_type)
+    return {'transactions': result, 'operation type': operation_type,
+            'date': 'all'}
 
 # Количество операций по заданному типу за заданный период (минута, 
 # час, день, неделя, месяц).
-@app.get('/count_tx/')
-def count_tx(operation_type:str='witness_reward'):
-    return helpers.api.get_tx_number(operation_type)
+@app.get('/count_tx/{operation_type}/{to_date_str}/{period_in_seconds}')
+def count_tx_by_op_type_in_period(operation_type:str="witness_reward",
+                                    to_date:dt.datetime=dt.datetime.now(),
+                                    period:dt.timedelta=dt.timedelta(hours=1)) -> dict:
+    to_date_str = dt.datetime.strftime(to_date, '%Y-%m-%d %H:%M:%S')
+    from_date_str = dt.datetime.strftime(to_date - period, '%Y-%m-%d %H:%M:%S')
+    period_in_seconds = period.seconds
+    result = helpers.api.get_tx_number_in_db_in_period(operation_type, to_date,
+                                                        period)
+    return {'transactions': result, 'operation type': operation_type,
+            'date': {'from': from_date_str, 'to': to_date_str}}
 
+# Количество SHARES, распределённых за всё время.
+@app.get('/shares/all')
+def shares_all() -> dict:
+    result = helpers.api.get_sum_shares_all()
+    return {'shares': result, 'operation type': 'all', 'date': 'all'}
+
+# Количество SHARES, распределенных в указанный период.
+@app.get('/shares/{to_date_str}/{period_in_seconds}')
+def shares_all_in_period(to_date:dt.datetime=dt.datetime.now(), 
+                        period:dt.timedelta=dt.timedelta(hours=1)) -> dict:
+    to_date_str = dt.datetime.strftime(to_date, '%Y-%m-%d %H:%M:%S')
+    from_date_str = dt.datetime.strftime(to_date - period, '%Y-%m-%d %H:%M:%S')
+    period_in_seconds = period.seconds
+    result = helpers.api.get_sum_shares_in_period(to_date, period)
+    return {'shares': result, 'operation type': 'all', 
+            'date': {'from': from_date_str, 'to': to_date_str}}
 
 # Количество распределенных SHARES по заданной операции за заданный 
 # период (минута, час, день, месяц).
-
+@app.get('/shares/{operation_type}/{to_date_str}/{period_in_seconds}')
+def shares_by_op_type_in_period(operation_type:str="witness_reward",
+                                to_date:dt.datetime=dt.datetime.now(),
+                                period:dt.timedelta=dt.timedelta(hours=1)) -> dict:
+    to_date_str = dt.datetime.strftime(to_date, '%Y-%m-%d %H:%M:%S')
+    from_date_str = dt.datetime.strftime(to_date - period, '%Y-%m-%d %H:%M:%S')
+    period_in_seconds = period.seconds
+    result = helpers.api.get_sum_shares_by_op_in_period(operation_type, 
+                                                        to_date, period)
+    return {'shares': result, 'operation type': operation_type, 
+            'date': {'from': from_date_str, 'to': to_date_str}}
+            
 # Вовлечённость за 7 дней. Самые награждаемые: посты в телеграм, 
 # авторы в телеграм, посты в Readdle.me, авторы в Readdle.me, 
 # категории в телеграм, категории в Readdle.me по заданному периоду 
