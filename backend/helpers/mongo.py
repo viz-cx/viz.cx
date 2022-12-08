ec5575bb6b281d2acc0094c09e5e9e66f79c519a
inov8 <blackisnewyellow@gmail.com>
2022-12-08 10:24:48 +0300

    Add save_block func.

--- backend/helpers/mongo.py
+++ backend/helpers/mongo.py
@@ -14,6 +14,17 @@ def save_transaction(tx):
     tx.update({'timestamp': tx_t})
     coll.insert_one(tx)
 
+def save_block(block):
+    """Save block to MongoDB collection."""
+    blocknumber = block[0]['block']
+    for tx in block:
+        tx.pop('block')
+        if tx.get('trx_id') == '0000000000000000000000000000000000000000':
+            tx.pop('trx_id')
+        tx_t = dt.datetime.fromisoformat(tx.get('timestamp'))
+        tx.update({'timestamp': tx_t})
+    coll.insert_one({'block {}'.format(blocknumber): block})
+    
 def get_last_block_in_db() -> list:
     """Return last block from collection in MongoDB database."""
     result = coll.find({}, {'_id': 0}).sort('block', pymongo.DESCENDING).limit(1)
