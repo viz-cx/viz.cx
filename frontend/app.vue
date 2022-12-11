4abb8053e3afe1201dda1996619ed0c5d6a70fd7
Vladimir Babin <vovababin@gmail.com>
2022-12-11 23:22:23 +0500

    Change default screen

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="min-h-screen w-screen pt-24">
+    <div class="max-w-7xl mx-auto">
+      <h1>{{ runtimeConfig.mainTitle }}</h1>
+    </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+const runtimeConfig = useRuntimeConfig()
+</script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      title: "DAO VIZ",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      script: [],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+        },
+      ],
+    },
+  },
+  runtimeConfig: {
+    public: {
+      mainTitle: "DAO VIZ"
+    }
+  }
+});
