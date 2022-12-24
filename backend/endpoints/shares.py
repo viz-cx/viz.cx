07f9e1762287d7415ccf1dcd0dca8c9126ca9b9c
inov8 <blackisnewyellow@gmail.com>
2022-12-24 20:02:52 +0300

    Add tg_stats and readdle.me stats

--- backend/endpoints/shares.py
+++ backend/endpoints/shares.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from unittest import result
 from fastapi import APIRouter
 import helpers.mongo as mongo
 
@@ -20,7 +21,7 @@ def sum_shares() -> dict:
 @router.get("/{to_date_str}/{from_date_str}")
 def sum_shares_in_period(
     to_date: dt.datetime = dt.datetime.now(),
-    from_date: dt.datetime = dt.datetime.now() - dt.timedelta(hours=1)
+    from_date: dt.datetime = dt.datetime.now() - dt.timedelta(hours=1),
 ) -> dict:
     to_date_str = dt.datetime.strftime(to_date, "%Y-%m-%d %H:%M:%S")
     from_date_str = dt.datetime.strftime(from_date, "%Y-%m-%d %H:%M:%S")
@@ -42,11 +43,13 @@ def sum_shares_by_op_type_in_period(
 ) -> dict:
     to_date_str = dt.datetime.strftime(to_date, "%Y-%m-%d %H:%M:%S")
     from_date_str = dt.datetime.strftime(from_date, "%Y-%m-%d %H:%M:%S")
-    result = mongo.get_sum_shares_by_op_in_period(operation_type, to_date, from_date)
+    result = mongo.get_sum_shares_by_op_in_period(
+        operation_type, to_date, from_date
+    )
     return {
         "shares": result,
         "operation_type": operation_type,
-        "date": {"from": from_date_str, "to": to_date_str}
+        "date": {"from": from_date_str, "to": to_date_str},
     }
 
 
@@ -56,10 +59,10 @@ def sum_shares_by_op_type_in_period(
     "/tg_stats/top_ch_posts/{to_date_str}/{from_date_str}/{in_top}/{to_skip}"
 )
 def show_top_tg_ch_posts_by_shares_in_period(
-    to_date: dt.datetime=dt.datetime.now(),
+    to_date: dt.datetime = dt.datetime.now(),
     from_date: dt.datetime = dt.datetime.now() - dt.timedelta(weeks=1),
-    in_top: int=5,
-    to_skip: int=0
+    in_top: int = 5,
+    to_skip: int = 0,
 ) -> dict:
     to_date_str = dt.datetime.strftime(to_date, "%Y-%m-%d %H:%M:%S")
     from_date_str = dt.datetime.strftime(from_date, "%Y-%m-%d %H:%M:%S")
@@ -67,9 +70,10 @@ def show_top_tg_ch_posts_by_shares_in_period(
         to_date, from_date, in_top, to_skip
     )
     return {
-        'tg posts with shares': result,
-        "date": {"from": from_date_str, "to": to_date_str}
+        "tg posts with shares": result,
+        "date": {"from": from_date_str, "to": to_date_str},
     }
+
 
 # Топ каналов в телеграм по полученным SHARES, выдаваемый с разными
 # настройками. По умолчанию, топ-5 за неделю.
@@ -77,10 +81,10 @@ def show_top_tg_ch_posts_by_shares_in_period(
     "/tg_stats/top_chs/{to_date_str}/{from_date_str}/{in_top}/{to_skip}"
 )
 def show_top_tg_channels_by_shares_in_period(
-    to_date: dt.datetime=dt.datetime.now(),
+    to_date: dt.datetime = dt.datetime.now(),
     from_date: dt.datetime = dt.datetime.now() - dt.timedelta(weeks=1),
-    in_top: int=5,
-    to_skip: int=0
+    in_top: int = 5,
+    to_skip: int = 0,
 ) -> dict:
     to_date_str = dt.datetime.strftime(to_date, "%Y-%m-%d %H:%M:%S")
     from_date_str = dt.datetime.strftime(from_date, "%Y-%m-%d %H:%M:%S")
@@ -88,27 +92,44 @@ def show_top_tg_channels_by_shares_in_period(
         to_date, from_date, in_top, to_skip
     )
     return {
-        'tg channels with shares': result,
-        "date": {"from": from_date_str, "to": to_date_str}
+        "tg channels with shares": result,
+        "date": {"from": from_date_str, "to": to_date_str},
     }
 
-# Топ постов в каналах в телеграм по количеству аварлов, выдаваемый с разными
+
+# Топ постов в каналах в телеграм по количеству авардов, выдаваемый с разными
 # настройками. По умолчанию, топ-5 за неделю.
 @router.get(
     "/tg_stats/top_chs_by_aws/{to_date_str}/{from_date_str}/{in_top}/{to_skip}"
 )
 def show_top_tg_ch_posts_by_awards_count_in_period(
-    to_date: dt.datetime=dt.datetime.now(),
+    to_date: dt.datetime = dt.datetime.now(),
     from_date: dt.datetime = dt.datetime.now() - dt.timedelta(weeks=1),
-    in_top: int=5,
-    to_skip: int=0
+    in_top: int = 5,
+    to_skip: int = 0,
 ) -> dict:
     to_date_str = dt.datetime.strftime(to_date, "%Y-%m-%d %H:%M:%S")
     from_date_str = dt.datetime.strftime(from_date, "%Y-%m-%d %H:%M:%S")
-    result = mongo.get_top_tg_ch_by_shares_in_period(
+    result = mongo.get_top_tg_ch_by_awards_in_period(
         to_date, from_date, in_top, to_skip
     )
     return {
-        'tg posts with awards': result,
-        "date": {"from": from_date_str, "to": to_date_str}
+        "tg posts with awards": result,
+        "date": {"from": from_date_str, "to": to_date_str},
     }
+
+
+# Топ каналов в телеграм по количеству авардов, выдаваемый с разными
+# настройками. По умолчанию, топ-5 за неделю.
+@router.get("/tg_stats/{tg_ch_id}/{from_date_str}/{to_date_str}")
+def show_tg_channel_awards_and_received_shares_in_period(
+    tg_ch_id: str = "@viz_news",
+    to_date: dt.datetime = dt.datetime.now(),
+    from_date: dt.datetime = dt.datetime.now() - dt.timedelta(weeks=1),
+) -> dict:
+    to_date_str = dt.datetime.strftime(to_date, "%Y-%m-%d %H:%M:%S")
+    from_date_str = dt.datetime.strftime(from_date, "%Y-%m-%d %H:%M:%S")
+    result = mongo.get_tg_ch_awards_and_shares_in_period(
+        tg_ch_id, to_date, from_date
+    )
+    return result
