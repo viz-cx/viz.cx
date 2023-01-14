5a03b72c12b18fd8749a5a6e5f32d923231368ac
Vladimir Babin <vovababin@gmail.com>
2023-01-14 12:31:31 +0500

    Add latest post link to profile

--- frontend/composables/viz.ts
+++ frontend/composables/viz.ts
@@ -1,16 +1,12 @@
-export async function getAccount(name: string): Promise<any> {
+export async function getAccount(name: string, custom_protocol_id: string = 'V'): Promise<any> {
   return new Promise((resolve, reject) => {
     const { $viz } = useNuxtApp()
-    $viz.api.getAccounts([name], (err: any, response: any) => {
+    $viz.api.getAccount(name, custom_protocol_id, (err: any, response: any) => {
       if (err) {
         reject(err)
         return
       }
-      if (response.length > 0) {
-        resolve(response[0])
-      } else {
-        reject(new Error(`Account ${name} not found in VIZ blockchain.`))
-      }
+      resolve(response)
     })
   })
 }
