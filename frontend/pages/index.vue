95b147d61e3485cc9ec3ba7f0782d0fe2caa3edb
Vladimir Babin <vovababin@gmail.com>
2024-01-15 21:56:01 +0300

    Add tabs values to url hash

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -8,7 +8,7 @@
                 <v-toolbar-title>{{ title }}</v-toolbar-title>
                 <template v-slot:extension>
                     <v-tabs v-model="tab" align-tabs="title">
-                        <v-tab v-for="item in tabs" :key="item" :value="item">
+                        <v-tab v-for="item in tabs" :key="item" :value="item" @click="updateRoute(item)">
                             {{ item }}
                         </v-tab>
                     </v-tabs>
@@ -21,7 +21,16 @@
 </template>
 
 <script setup lang="ts">
+const router = useRouter()
 const title = 'Voice Protocol Posts'
 const tab = ref<string | null>(null)
 const tabs = ['newest', 'popular']
+const tabFromRouteHash = router.currentRoute.value.hash.replace('#', '')
+if (tabs.includes(tabFromRouteHash)) {
+    tab.value = tabFromRouteHash
+}
+
+function updateRoute(tab: string | null) {
+    router.replace({ 'hash': '#' + tab })
+}
 </script>
